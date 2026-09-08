e53cc13aedf3fd17c0b7f78e0f614ebc50a6830a
Jeff Rogers <jbrogers63@gmail.com>
2026-09-08 10:54:02 -0400

    Abstract local configurations out to their own conf directory.

---
+++ conf/default.cmake
@@ -0,0 +1,10 @@
+set(AKROGON_BUILD_TRIPLE
+	"x86_64-akrogon-linux-gnu"
+	CACHE STRING "Akrogon build triple"
+)
+
+set(AKROGON_TARGET_TRIPLE
+	"x86_64-akrogon-linux-gnu"
+	CACHE STRING "Akrogon target triple"
+)
+
